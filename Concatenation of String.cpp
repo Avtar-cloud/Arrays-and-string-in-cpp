c6e171eb97fd23c93f5834a7199491d360b42c34
Avtar-cloud <avtar.singh.btech2024@sitpune.edu.in>
2025-08-27 14:43:25 +0530

    Create Concatenation of String.cpp

---
+++ Concatenation of String.cpp
@@ -0,0 +1,26 @@
+// Avtar singh
+// 24070123027
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    string str1;
+    string str2;
+    string str3;
+    
+    cout<<"Enter first string\n";
+    cin>>str1;
+    cout<<"Enter second string\n";
+    cin>>str2;
+    str3=str1+" "+str2;
+    cout<<"Concatenated string is "<<str3;
+    return 0;
+}
+
+// OUTPUT
+// Enter first string
+// avtar
+// Enter second string
+// singh
+// Concatenated string is Aditya Jadhav
