08c755a5b1260d4ed94a129c00e1058ed4afd7a7
Avtar-cloud <avtar.singh.btech2024@sitpune.edu.in>
2025-08-27 14:47:49 +0530

    Create Printing String.cpp

---
+++ Printing String.cpp
@@ -0,0 +1,18 @@
+// avtar singh
+// 24070123005
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    string str1;
+    cout<<"Enter a string\n";
+    cin>>str1;
+    cout<<"Entered string is "<<str1;
+    return 0;
+}
+
+// OUTPUT
+// Enter a string
+// avtar
+// Entered string is avtar
